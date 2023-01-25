4469c504c7b3c7db53ec6f0e3c904960fa4398ed
Shaker Ahamed <ahamedshaker7@gmail.com>
2023-01-25 12:35:51 +0600

    navber is done

---
+++ src/pages/home/navbar.jsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import websiteLogo from '../../assets/Images/portfolio-logo.png'
+
+const Navbar = () => {
+    return (
+        <header className='w-{100%} h-[120px] bg-secondary-600 flex justify-around items-center'>
+            <Link href={"/"}>
+                <Image src={websiteLogo} width={226} height={56} alt="website-logo" />
+            </Link>
+            <div className='flex justify-center items-center gap-[50px]'>
+                <nav className='flex list-none gap-[30px]'>
+                    <Link href={"/"}><li className='text-gray-600 font-medium text-base leading-[19px] hover:text-black-600'>Home</li></Link>
+                    <Link href={"/"}><li className='text-gray-600 font-medium text-base leading-[19px] hover:text-black-600'>About</li></Link>
+                    <Link href={"/"}><li className='text-gray-600 font-medium text-base leading-[19px] hover:text-black-600'>Services</li></Link>
+                    <Link href={"/"}><li className='text-gray-600 font-medium text-base leading-[19px] hover:text-black-600'>Projects</li></Link>
+                </nav>
+                <button className='h-[54px] w-[170px] text-primary-600 bg-gradient-to-r from-red-500 to-red-600 border-2 border-gradient-to-r from-red-500 to-red-600 rounded-full shadow-[0px 15px 30px 0px #C63B2321]'>
+                    HIRE ME
+                </button>
+            </div>
+        </header >
+    ); 
+};
+
+export default Navbar;
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      primary:{
+        "600": "#FF4444"
+      },
+      secondary:{
+        "600": "#AFB5C01A"
+      },
+      black: {
+        "600": "#0B132A"
+      },
+      gray: {
+        "600": "#4F5665"
+      }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
